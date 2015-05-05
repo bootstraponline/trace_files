28401d321064486971747a96726ac3136cddc915
bootstraponline <code@bootstraponline.com>
2015-05-05 17:12:00 -0400

    Release 0.0.2

--- lib/trace_files/version.rb
+++ lib/trace_files/version.rb
@@ -1,4 +1,4 @@
 module TraceFiles
-  VERSION = '0.0.1' unless defined? ::TraceFiles::VERSION
+  VERSION = '0.0.2' unless defined? ::TraceFiles::VERSION
   DATE    = '2015-05-05' unless defined? ::TraceFiles::DATE
 end
